b2b75fcffc9edb47e848686fa6605b77afd25a01
Ricardo Ferreira <ricardoferreiran@gmail.com>
2025-06-23 21:39:36 +0200

    se le agrego logica a las funciones

--- src/Components/ContadorComponent.jsx
+++ src/Components/ContadorComponent.jsx
@@ -1,9 +1,25 @@
 import { useState } from 'react'
 
 export const ContadorComponent = () => {
+
+  const [valor, setValor] = useState(0)
+
+  const incrementar = () => {
+    setValor( valor + 1 )
+  }
+
+  const decrementar = () => {
+    if ( valor < 1 ) return
+    setValor( valor - 1 )
+  }
+
+  const resetear = () => {
+    setValor(0)
+  }
+
   return (
     <>
-      <h1>Contador: 0</h1>
+      <h1>Contador: { valor }</h1>
       <button onClick={ incrementar }>+1</button>
       <button onClick={ resetear }>Reset</button>
       <button onClick={ decrementar }>-1</button>
